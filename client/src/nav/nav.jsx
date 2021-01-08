a8bba2cdef28e375ff0bf7dcf080a62e60e47af1
jwnukoski <jwnukoski@protonmail.com>
2021-01-08 01:31:53 -0500

    styling

--- client/src/nav/nav.jsx
+++ client/src/nav/nav.jsx
@@ -4,15 +4,8 @@ import styles from './css/nav.module.css'
 
 function Nav () {
   return (
-		<div className="container-fluid">
-      <div className="row">
-        <div className="col-lg-11 col-md-10 col-sm-12 col-12">
-          <span className={styles.branding}>Crypto-Charts</span>
-        </div>
-        <div className="col-lg-1 col-md-2 col-sm-12 col-12">
-
-        </div>
-      </div>
+    <div className={styles.navWrapper}>
+      <span className={styles.branding}>Crypto-Charts</span>
     </div>
   )
 }
