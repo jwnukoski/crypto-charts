98810fb22f6e947243fa68de75d118a99d583d8b
jwnukoski <jwnukoski@protonmail.com>
2021-02-13 15:26:01 -0500

    production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: path.resolve(__dirname, './client/src/index.jsx'),
   module: {
     rules: [
